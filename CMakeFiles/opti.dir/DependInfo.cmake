2425d4ac2a9bc4f659fc75df42230d07fb3df34d
qnie <qnie@princeton.edu>
2017-05-25 23:29:04 -0400

    add cmakefiles

---
+++ CMakeFiles/opti.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/u/qnie/research/MappingOpti/ALG/src/Application.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/Application.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/CGScheduling.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/CGScheduling.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/ComputationGraph.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/ComputationGraph.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/DRAMAccess.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/DRAMAccess.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/Data.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/Data.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/DataBlock.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/DataBlock.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/Hardware.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/Hardware.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/MacroNode.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/MacroNode.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/MacroNodeTemplate.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/MacroNodeTemplate.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/Memory.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/Memory.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/OptiMacroNode.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/OptiMacroNode.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/Pattern.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/Pattern.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/ProcessConfig.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/ProcessConfig.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/Scheduling.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/Scheduling.cpp.o"
+  "/u/qnie/research/MappingOpti/ALG/src/main.cpp" "/u/qnie/research/MappingOpti/ALG/CMakeFiles/opti.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
